4c518f278f3935673c77ea6d2ef9166f471da431
Nicholas <me@cheeze2000.net>
2020-09-01 23:38:37 +0800

    feat(Utils): reverse function

--- src/Lizz/Utils.hs
+++ src/Lizz/Utils.hs
@@ -1,6 +1,6 @@
 module Lizz.Utils where
 
-import Prelude hiding (map)
+import Prelude hiding (map, reverse)
 import Lizz.Internal
 
 flatten :: Int -> Lizz -> Lizz
@@ -14,3 +14,12 @@ map :: (String -> String) -> Lizz -> Lizz
 map _ [] = []
 map f (Lizz' x xs : ys) = l : map f ys
   where l = Lizz' (f x) $ map f xs
+
+reverse :: Lizz -> Lizz
+reverse [] = []
+reverse (x:xs) = go [] (x:xs)
+  where
+    go xs [] = xs
+    go xs (Lizz' y ys : zs) =
+      let l = Lizz' y (reverse ys)
+      in go (l:xs) zs
