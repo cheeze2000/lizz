463d99272b7dba369d18cd3c6c0481aef4411f51
Nicholas <me@cheeze2000.net>
2020-09-01 23:26:42 +0800

    feat(Utils): flatten function

--- src/Lizz/Utils.hs
+++ src/Lizz/Utils.hs
@@ -3,6 +3,13 @@ module Lizz.Utils where
 import Prelude hiding (map)
 import Lizz.Internal
 
+flatten :: Int -> Lizz -> Lizz
+flatten _ [] = []
+flatten 0 xs = xs
+flatten n (Lizz' x xs : ys) = l ++ flatten n ys
+  where
+    l = Lizz' x [] : flatten (n - 1) xs
+
 map :: (String -> String) -> Lizz -> Lizz
 map _ [] = []
 map f (Lizz' x xs : ys) = l : map f ys
